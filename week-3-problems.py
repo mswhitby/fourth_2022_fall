5c425a51f64ee0e820b8e5969be1818365305428
deandrawhitby <104539455+deandrawhitby@users.noreply.github.com>
2022-08-29 14:31:20 -0500

    Update week-3-problems.py

--- week-3-problems.py
+++ week-3-problems.py
@@ -1,5 +1,4 @@
 # Adding Numbers
-
 def adding(number1, number2):
   return number1 + number2
   
@@ -7,3 +6,18 @@ number1 = int(input("Enter a number: "))
 number2 = int(input("Enter another number: "))
 answer = adding(number1, number2)
 print(answer)
+
+# List of Multiples
+def list_of_multiples(num, length):
+  multiple_list = []
+  for value in range(1, length+1):
+    multiple = num * value
+    multiple_list.append(multiple)
+    # print(f"{num} * {value} = {multiple}")
+    
+  return multiple_list
+    
+num = int(input("Enter a number: "))
+length = int(input("Enter another number: "))
+ans = my_function(num, length)
+print(ans)
